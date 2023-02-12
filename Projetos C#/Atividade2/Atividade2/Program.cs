e6e831bac068b50ccf0be2ab8aeb0e856b8e0e7f
Lucas Silva <lucasdssilva13@hotmail.com>
2023-02-12 16:50:53 -0300

    Atividade 2
    Atividade 2

---
+++ Projetos C#/Atividade2/Atividade2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CodigoSplit;
+using System.Globalization;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Entre com seu nome completo: ");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Quantos quartos tem na sua casa? ");
+        int quartos = int.Parse(Console.ReadLine());
+        Console.WriteLine("Entre com o preço de um produto: ");
+        double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha) ");
+        string[] vet = Console.ReadLine().Split(' ');
+        string nome2 = vet[0];
+        int idade = int.Parse(vet[1]);
+        double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+        Console.WriteLine("Você digitou: ");
+        Console.WriteLine(nome);
+        Console.WriteLine(quartos);
+        Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine(nome2);
+        Console.WriteLine(idade);
+        Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
