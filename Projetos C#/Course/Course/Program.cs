48d3702447f9e0b06b812b1019e142331bb8b35a
Lucas Silva <lucasdssilva13@hotmail.com>
2023-02-12 16:21:25 -0300

    Primeiro commit
    Primeiro commit

---
+++ Projetos C#/Course/Course/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:f1} reais", nome, idade, saldo);
+
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:f1} reais ");
+
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("f1",CultureInfo.InvariantCulture ) + " reais ");
+        }
+    }
+}
