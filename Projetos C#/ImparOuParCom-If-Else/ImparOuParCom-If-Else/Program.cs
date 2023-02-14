b0e20abe2da143d5afe350607de0b666575b476a
Lucas Silva <lucasdssilva13@hotmail.com>
2023-02-14 17:17:35 -0300

    if e else commit

---
+++ Projetos C#/ImparOuParCom-If-Else/ImparOuParCom-If-Else/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace If_Else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com um numero inteiro: ");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("Par! ");
+            }
+            else
+            {
+                Console.WriteLine("Impar! ");
+            }
+
+        }
+    }
+}
