761a4afb2e4ac5eb09301bd4365ba554c4845e14
Lucas Silva <lucasdssilva13@hotmail.com>
2023-02-14 17:31:46 -0300

    bom dia com if e else commit

---
+++ Projetos C#/BomDiaIFeELSE/BomDiaIFeELSE/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace BomDiaIFeELSE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o hora atual? ");
+            int hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12)
+            {
+                Console.WriteLine("Bom dia! ");
+            }
+            else if (hora < 18)
+            {
+                Console.WriteLine("Boa tarde! ");
+            }
+            else
+            {
+                Console.WriteLine("Boa noite! ");
+            }
+        }
+    }
+}
